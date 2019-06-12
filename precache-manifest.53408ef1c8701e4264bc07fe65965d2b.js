a1405c5afc0303c88f4af817a1ec3368b28d3017
gregatgit <gregdd@outlook.com>
2019-06-12 10:14:07 +1000

    Updates

---
+++ precache-manifest.53408ef1c8701e4264bc07fe65965d2b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ad64401e6ea8c83e0b48bc62b63318",
+    "url": "/how-to-vote/index.html"
+  },
+  {
+    "revision": "4df350b889ff146057ad",
+    "url": "/how-to-vote/static/css/main.1cdceed7.chunk.css"
+  },
+  {
+    "revision": "171a5cbdbc067215463d",
+    "url": "/how-to-vote/static/js/2.4b18cf0d.chunk.js"
+  },
+  {
+    "revision": "4df350b889ff146057ad",
+    "url": "/how-to-vote/static/js/main.9123bf86.chunk.js"
+  },
+  {
+    "revision": "e15faecf9686a48ee21c",
+    "url": "/how-to-vote/static/js/runtime~main.fb6c466b.js"
+  },
+  {
+    "revision": "3af178ecd64adfa098e7456e5830e5f8",
+    "url": "/how-to-vote/static/media/Game_of_Thrones.3af178ec.ttf"
+  },
+  {
+    "revision": "19cf08662164a41c6a270ddbbc4c9648",
+    "url": "/how-to-vote/static/media/TrajanusBriX-Invers.19cf0866.ttf"
+  },
+  {
+    "revision": "618e11b487bfa6845aaa1bd111239c95",
+    "url": "/how-to-vote/static/media/TrajanusBricks.618e11b4.ttf"
+  },
+  {
+    "revision": "80ec6203553376a44fafb15d32b8e6b9",
+    "url": "/how-to-vote/static/media/TrajanusBricksXtra.80ec6203.ttf"
+  }
+]);
